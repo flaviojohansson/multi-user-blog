ce418fdbc9725a29fe14acda039d82e43b8c10cd
Flavio D Johansson <flaviodaniel@outlook.com>
2016-12-06 22:14:37 -0200

    Add check if comment exists

--- app/edit_comment.py
+++ app/edit_comment.py
@@ -51,6 +51,10 @@ class EditComment(BaseHandler):
         key = db.Key.from_path('Comment', int(comment_id))
         comment = db.get(key)
 
+        if not comment:
+            self.error(404)
+            return
+
         # Make sure the logged user is the owner of the comment
         if self.user.name != comment.user_name:
             self.redirect('/post/%s' % str(post_id))
