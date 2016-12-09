917b62daf10d418f9c9c37fd1000ffbecfe502b9
Flavio D Johansson <flaviodaniel@outlook.com>
2016-12-08 22:15:27 -0200

    Remove unreachable code

--- app/handlers/edit_comment.py
+++ app/handlers/edit_comment.py
@@ -14,26 +14,9 @@ class EditComment(BaseHandler):
     @check_if_valid("Comment")
     @check_if_owner("Comment")
     def get(self, post_id, comment_id):
-        # Not logged users are redirect to the login page and then
-        # redirected back here
-        if not self.user:
-            self.redirect('/login?redirect=' +
-                          urllib.pathname2url('/comment/edit/{}/{}'.format(
-                              [str(post), str(comment_id)]
-                          )))
-            return
 
         key = db.Key.from_path('Comment', int(comment_id))
         comment = db.get(key)
-
-        if not comment:
-            self.error(404)
-            return
-
-        # Make sure the logged user is the owner of the post
-        if self.user.key() != comment.user.key():
-            self.redirect('/post/%s' % str(post_id))
-            return
 
         self.render('edit_comment.html',
                     post_id=post_id,
@@ -49,23 +32,10 @@ class EditComment(BaseHandler):
     @check_if_valid("Comment")
     @check_if_owner("Comment")
     def post(self, post_id, comment_id):
-        # Not logged users are redirected to the login page
-        if not self.user:
-            self.redirect('/login')
-            return
 
         # Get the comment itself
         key = db.Key.from_path('Comment', int(comment_id))
         comment = db.get(key)
-
-        if not comment:
-            self.error(404)
-            return
-
-        # Make sure the logged user is the owner of the comment
-        if self.user.key() != comment.user.key():
-            self.redirect('/post/%s' % str(post_id))
-            return
 
         # When the user clicks on the delete button the action becomes 'delete'
         if self.request.get('action') == 'delete':
